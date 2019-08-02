48843ecfbd5a6339d63c75619f9790fb2a5a330a
Alice Brunel <alc.brunel@gmail.com>
2019-08-02 21:24:43 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,13 +1,25 @@
 class Owner
   attr_reader :name, :species
   
+  @@all = []
+  
   def initialize(name)
     @name = name
     @species = "human"
+    @@all << self
   end
   
   def say_species
     specy = self.species
     "I am a #{specy}."
   end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.count 
+    @@all.count
+  end
+  
 end
