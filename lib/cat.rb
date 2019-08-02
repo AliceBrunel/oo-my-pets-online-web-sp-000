48cdc1b990bec1ced58db5e1289cadca5b5984a6
Alice Brunel <alc.brunel@gmail.com>
2019-08-02 21:29:20 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,12 +1,19 @@
 class Cat
   
+  @@all = []
+  
   attr_reader :name
   attr_accessor :mood, :owner
   
   def initialize(name, owner)
     @name = name
     @owner = owner
-    @mood = "nervous mood"
+    @mood = "nervous"
+    @@all << self
+  end
+  
+  def self.all
+    @all
   end
   
 end
